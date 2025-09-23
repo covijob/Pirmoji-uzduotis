3448c877217d3ea939069e7f042e8217fdebbef3
Mangirdas <mangirdas.gudas@gmail.com>
2025-09-23 14:30:59 +0300

    Sukurtas pasirinkimas vartotojui atsitiktinai generuoti studentų duomenis.

--- Pirmoji-uzduotis.cpp
+++ Pirmoji-uzduotis.cpp
@@ -6,7 +6,7 @@
 #include <limits>
 #include <sstream>
 #include <algorithm>
-
+#include <random>
 
 struct Studentas {
 	std::string vardas;
@@ -15,6 +15,7 @@ struct Studentas {
 	int egzaminas = 0;
 	double galutinis = 0.0;
 };
+
 double vidurkis(const std::vector<int>& a) {
 	if (a.empty()) return 0.0;
 	long long s = 0;
@@ -41,7 +42,7 @@ double galutinis_mediana(const Studentas& s) {
 std::size_t u8len(const std::string& s) {
 	std::size_t n = 0;
 	for (unsigned char c : s) {
-		if ((c & 0xC0) != 0x80) ++n;  
+		if ((c & 0xC0) != 0x80) ++n;
 	}
 	return n;
 }
@@ -54,15 +55,65 @@ void print_col(std::ostream& out, const std::string& text, std::size_t width) {
 //-----------------
 
 int main() {
+	const char* pr = "studentai1.txt";
 
 	int vartotojo_pasirinkimas = 1;
 	std::cout << "Pasirinkite skaiciavimo buda (1 - vidurkis, 2 - mediana): ";
 	if (!(std::cin >> vartotojo_pasirinkimas) || (vartotojo_pasirinkimas != 1 && vartotojo_pasirinkimas != 2)) {
-		std::cout << "Neteisingas pasirinkimas. Naudojamas vidurkis.\n";
-		vartotojo_pasirinkimas = 1;
+		std::cout << "Prasome pasirinkti 1 arba 2.\n";
+		std::cout << "Programa nutraukiama.\n";
+		std:exit(1);
+		
 	}
 
-	const char* pr = "studentai1.txt";
+	int pasirinktas_saltinis = 1;
+	std::cout << "Pasirinkite duomenu saltini (1 - txt failas, 2 - atsitiktinai generuoti domenys): ";
+	if (!(std::cin >> pasirinktas_saltinis) || (pasirinktas_saltinis != 1 && pasirinktas_saltinis != 2)){
+		std::cout << "Prasome pasirinkti 1 arba 2.\n";
+		std::cout << "Programa nutraukiama.\n";
+		std::exit(1);
+	}
+	// txt generavimas 
+	if (pasirinktas_saltinis == 2) {
+		std::mt19937 rng(std::random_device{}());
+		std::uniform_int_distribution<int> distN(1, 100000);
+		std::uniform_int_distribution<int> distK(1, 25);
+
+		int N = distN(rng); //studentu skaicius
+		int K = distK(rng); //pazymiu skaicius studentui
+
+		//std::cout << N << " " << K << "\n";
+
+		int minP = 1;
+		int maxP = 10;
+
+		pr = "studentai_gen.txt";
+
+		std::ofstream gen(pr);
+
+		if (!gen) {
+			std::cout << "Nepavyko sukurti failo: " << pr << std::endl;
+			return 4;
+		} // note self:  gen tampa ofstream objektu, eina konversija i bool tipa
+
+		gen << "Pavarde Vardas ";
+		for (int j = 1; j <= K; ++j) gen << " ND" << j;
+		gen << " Egzaminas\n";
+
+		std::uniform_int_distribution<int> distPaz(minP, maxP);
+
+		for (int i = 1; i <= N; ++i) {
+			gen << "Pavarde" << i << " Vardas" << i;
+			for (int j = 1; j <= K; ++j) gen << " " << distPaz(rng);
+			gen << " " << distPaz(rng) << "\n";
+		}
+		gen.close();
+
+	}
+	// txt generavimo pabaiga
+
+
+	//const char* pr = "studentai1.txt";
 	const char* rz = "rezultatas.txt";
 
 	std::ifstream in(pr);
@@ -93,7 +144,7 @@ int main() {
 		while (iss >> x) visi_skaiciai.push_back(x);
 
 		if (visi_skaiciai.empty()) {
-			std::cout << nr << " eiluteje nerasta neivieno pazymio - studentas praleidziamas.\n";
+			std::cout << nr << " eiluteje nerasta nei vieno pazymio - studentas praleidziamas.\n";
 			continue;
 
 		}
